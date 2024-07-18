a0f13ce37862e63f6c81389cf832801e5854a7f5
ANGELO AUGUSTO LEAL FERREIRA DE SOUZA <angeloaugusto@fieg.com.br>
2024-07-18 10:10:38 -0300

    teste

---
+++ Anotações/funcao.js
@@ -0,0 +1,5 @@
+function imprimirSoma(a, b){
+    console.log(a + b)
+}
+imprimirSoma(2, 3)
+imprimirSoma(2)
---
+++ Anotações/funcao1.js
@@ -0,0 +1,11 @@
+
+const imprimirSoma = function (a, b){
+    console.log(a + b)
+}
+imprimirSoma(2, 3)
+const soma = (a, b) => {
+    return a + b
+}
+console.log(soma(2, 3))
+const subtracao = (a, b) => a -b
+console.log(subtracao(2, 3))
